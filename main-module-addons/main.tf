8ce557be6d51fa6f5ec0b392c787b50d790649b4
Jungas80 <juanhegert@gmail.com>
2022-05-08 22:33:36 -0300

    Add outputs

--- main-module-addons/main.tf
+++ main-module-addons/main.tf
@@ -1,19 +1,14 @@
-resource "random_string" "random" {
-    length = 8
-}
-
-data "aws_security_group" "vpc_web" {
-  id = "sg-0321a9d9816cb6dde"
-}
-
-
-#id=vpc-0fa9670bec2a00044
 provider "aws" {
   region = "us-east-1"
 }
+
+data "aws_security_group" "vpc_web" {
+  id = "sg-03bf042e30040a04e"
+}
+
 resource "aws_instance" "test_ec2" {
-    ami = "ami-0022f774911c1d690"
-    instance_type = "t2.micro"
-    subnet_id = "subnet-064d36de3fb654feb"
-    vpc_security_group_ids = [data.aws_security_group.vpc_web.id]
+  ami                    = "ami-0022f774911c1d690"
+  instance_type          = "t2.micro"
+  subnet_id              = "subnet-0168f78ded14b6ec5"
+  vpc_security_group_ids = [data.aws_security_group.vpc_web.id]
 }
---
+++ mainmodule/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "Output the ID of the primary VPC"
+  value       = aws_vpc.vpc.id
+}
+
+output "vpc_information" {
+  description = "VPC information about Environment"
+  value       = "Your ${aws_vpc.vpc.tags.Environment}"
+}
+
+output "aws_security_group" {
+  description = "Show your SG id port 80"
+  value       = "Your SG id is ${aws_security_group.vpc-web.id}"
+}
